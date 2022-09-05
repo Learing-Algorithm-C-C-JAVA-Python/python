362149d6a60cb0057377a5d86c2477757031ecf9
HUAWEI-Developer <79436937+panhongsheng-eng@users.noreply.github.com>
2022-09-05 17:22:24 +0800

    Create SelectionSort.py

---
+++ SelectionSort.py
@@ -0,0 +1,16 @@
+#待排序序列
+list = [14,33,27,10,35,19,42,44]
+
+def selection_sort():
+    length = len(list)
+    for i in range(length-1):
+        min = i
+        for j in range(i+1,length):
+            if list[j] < list[min]:
+                min = j
+        if min != i:
+            list[i],list[min] = list[min],list[i]
+selection_sort()
+
+for i in list:
+    print(i,end=" ")
